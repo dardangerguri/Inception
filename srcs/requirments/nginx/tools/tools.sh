971268cc5c333715bfc84398d3112df2754b6479
Dardan <dardangerguri@outlook.com>
2024-03-13 17:25:22 +0200

    cleaned the dockerfile and added some comments, changed the config file and also added comments

--- srcs/requirments/nginx/tools/tools.sh
+++ srcs/requirments/nginx/tools/tools.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048\
-		-out /etc/nginx/ssl/inception.crt\
-		-keyout /etc/nginx/ssl/inception.key\
-		-subj "/C=FI/ST=Helsinki/L=Helsinki/O=Hive Helsinki/OU=dgerguri/CN=dgerguri.42.fr/UID=dgerguri"
-
+# Generate SSL certificate and key if they don't exist
+if [ ! -f /etc/nginx/ssl/inception.crt ] || [ ! -f /etc/nginx/ssl/inception.key ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -out /etc/nginx/ssl/inception.crt \
+        -keyout /etc/nginx/ssl/inception.key \
+        -subj "/C=FI/ST=Helsinki/L=Helsinki/O=Hive Helsinki/OU=dgerguri/CN=dgerguri.42.fr/UID=dgerguri"
+fi
+# Start NGINX
 nginx -g "daemon off;"
-
