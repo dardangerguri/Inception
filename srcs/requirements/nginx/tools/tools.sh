e25a4da85aa3ed531a7ecb33f36e4f0d6be0fba8
Dardan <dardangerguri@outlook.com>
2024-03-19 16:45:59 +0200

    modified the config file so the wp-admin works, and also modified the variables on the script

--- srcs/requirements/nginx/tools/tools.sh
+++ srcs/requirements/nginx/tools/tools.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Generate SSL certificate and key if they don't exist
-if [ ! -f /etc/nginx/ssl/inception.crt ] || [ ! -f /etc/nginx/ssl/inception.key ]; then
+if [ ! -f ${CERT_} ] || [ ! -f ${CERT_KEY} ]; then
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -out /etc/nginx/ssl/inception.crt \
-        -keyout /etc/nginx/ssl/inception.key \
+        -out ${CERT_} \
+        -keyout ${CERT_KEY} \
         -subj "/C=FI/ST=Helsinki/L=Helsinki/O=Hive Helsinki/OU=student/CN=dgerguri.42.fr"
 fi
 # Start NGINX
